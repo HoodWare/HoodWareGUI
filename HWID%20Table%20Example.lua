44d660ddcdc6c81546cc629c0eafb55e0ab44147
HoodWare <112295247+HoodWare@users.noreply.github.com>
2024-02-17 06:51:45 -0600

    Update HWID%20Table%20Example.lua

--- HWID%20Table%20Example.lua
+++ HWID%20Table%20Example.lua
@@ -1,10 +1,4 @@
 return {
 	"922a05ab-c0a1-4e5e-869b-5b31af6abc91", -- emmanuel.17
-	"3fe38233-3afc-4843-a0f0-ba28a01a7ae6", -- ME,
-	"sad67das6das67dasd6",
-	},
-	"d3ed8y12e71e11", -- HWID
-	"testkobe", -- UserInfo
-]
-	-- extrastuff
+	"3fe38233-3afc-4843-a0f0-ba28a01a7ae6", -- ME
 }
