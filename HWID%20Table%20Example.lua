e2d2039d0d4d3ff713e6a5a952defe61a3474cf0
HoodWare <112295247+HoodWare@users.noreply.github.com>
2024-02-17 08:28:06 -0600

    Update HWID%20Table%20Example.lua

--- HWID%20Table%20Example.lua
+++ HWID%20Table%20Example.lua
@@ -1,8 +1,4 @@
 return {
 	"922a05ab-c0a1-4e5e-869b-5b31af6abc91", -- 1060621903913566318
 	"3fe38233-3afc-4843-a0f0-ba28a01a7ae6", -- ME
-"whad", -- test
 }
-
--- BlackListed Users
-"test", -- No additional info provided
