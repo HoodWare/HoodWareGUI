e2791da149261dea6aa2851c016d4122001e611d
HoodWare <112295247+HoodWare@users.noreply.github.com>
2024-02-17 06:24:28 -0600

    Update file

--- HWID%20Table%20Example.lua
+++ HWID%20Table%20Example.lua
@@ -18,4 +18,6 @@ return {
 	"922a05ab-c0a1-4e5e-869b-5b31af6abc91", -- emmanuel.17
 	"3fe38233-3afc-4843-a0f0-ba28a01a7ae6", -- ME
 },
-	"dasasre23323",
+	"dasasre23323",,
+	"324234234234234test",
+}
